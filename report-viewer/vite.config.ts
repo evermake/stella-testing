7bc9c1c58cf521ef6e6d3b441eb6d0a03e3e7050
evermake <vladeryabkin@gmail.com>
2024-02-07 16:22:22 +0300

    fix github pages path

--- report-viewer/vite.config.ts
+++ report-viewer/vite.config.ts
@@ -1,6 +1,8 @@
+import process from "node:process"
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.VITE_BASE_URL ?? '/',
 })
