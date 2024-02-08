5140d2ef90c4f3fe9faa89df6a0b39a0b10a7e7f
evermake <vladeryabkin@gmail.com>
2024-02-08 10:34:32 +0300

    update viewer to support new report format

--- report-viewer/src/types.ts
+++ report-viewer/src/types.ts
@@ -1,5 +1,7 @@
 import type { Report as ReportOriginal_ } from "../../scripts/types"
 
+export type { TestcaseConclusion } from "../../scripts/types"
+
 export type ReportOriginal = ReportOriginal_
 export type Report = Omit<ReportOriginal, "testcases"> & {
   testcases: (ReportOriginal["testcases"][number] & {
